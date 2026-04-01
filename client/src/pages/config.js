43d9fe7581aa104b224bd07cb70e9c9167496e41
Amit Patidar <amit.patidar6264@gmail.com>
2026-04-01 23:39:37 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,8 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://backend-703570644.eu-north-1.elb.amazonaws.com"
+//const API_BASE_URL = "http://backend-703570644.eu-north-1.elb.amazonaws.com"
 
 //const API_BASE_URL = "http://amitshop.shop"
-//const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
+const API_BASE_URL = "http://13.60.157.141:81";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
