708e5b2b1be79aacac46e032c6e46f45156f004f
Amit Patidar <amit.patidar6264@gmail.com>
2026-04-02 00:17:30 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -2,7 +2,7 @@
 //const API_BASE_URL = "http://backend-703570644.eu-north-1.elb.amazonaws.com"
 
 //const API_BASE_URL = "http://amitshop.shop"
-const API_BASE_URL = "http://13.60.157.141:84";
+const API_BASE_URL = "http://16.16.91.37:84";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
